4d7c5f6c31fb5fb79403a062de42f205624b1705
Colin Orr <colin.orr@gmail.com>
2017-04-14 15:43:10 +0100

    Layout the days

--- src/components/Calendar/Month/Day/index.js
+++ src/components/Calendar/Month/Day/index.js
@@ -3,11 +3,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import format from 'date-fns/format'
 
-const Day = (props) => (
-  <div className='Day'>
-    {format(props.value, 'DD/MM/YYYY')}
-  </div>
-)
+const Day = (props) => {
+
+  let dateFormat = 'D'
+  if (props.value.getDate() === 1) dateFormat = 'D MMM'
+
+  return (
+    <div className={`Day Day-${format(props.value, 'ddd')}`}>
+      {format(props.value, dateFormat)}
+    </div>
+  )
+}
 
 Day.PropTypes = {
   value: PropTypes.instanceOf(Date).isRequired,
